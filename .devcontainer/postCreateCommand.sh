9ff2be3a8ae3ac599e5cc77cc78d313e9860fe21
Josiah Siegel <5522990+JosiahSiegel@users.noreply.github.com>
2024-03-04 23:38:21 +0000

    add post create commands

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+apt update
+apt install -y libwebkit2gtk-4.0-dev \
+    build-essential \
+    curl \
+    wget \
+    file \
+    libssl-dev \
+    libgtk-3-dev \
+    libayatana-appindicator3-dev \
+    librsvg2-dev
